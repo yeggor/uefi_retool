9b17349ad5c76761b875a942ccbf9882440476a5
yeggor <vasilenko.yegor@gmail.com>
2019-08-28 05:28:28 +0300

    display of a window with protocols added

--- ida_plugin/uefi_analyser/utils.py
+++ ida_plugin/uefi_analyser/utils.py
@@ -2,15 +2,20 @@ import idautils
 import idaapi
 import idc
 
+"""
+definitions from PE file structure
+"""
 IMAGE_FILE_MACHINE_IA64 = 0x8664
 IMAGE_FILE_MACHINE_I386 = 0x014c
 PE_OFFSET = 0x3c
-
 IMAGE_SUBSYSTEM_EFI_APPLICATION = 0xa
 IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER = 0xb
 IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER = 0xc
 
 def set_hexrays_comment(address, text):
+    """
+    set comment in decompiled code
+    """
     cfunc = idaapi.decompile(address)
     tl = idaapi.treeloc_t()
     tl.ea = address
@@ -19,10 +24,15 @@ def set_hexrays_comment(address, text):
     cfunc.save_user_cmts()
 
 def check_guid(address):
-    # determined by the number of unique bytes
+    """
+    correctness is determined based on the number of unique bytes
+    """
     return (len(set(idaapi.get_many_bytes(address, 16))) > 8)
 
 def get_guid(address):
+    """
+    get GUID located by address
+    """
     CurrentGUID = []
     CurrentGUID.append(idc.Dword(address))
     CurrentGUID.append(idc.Word(address + 4))
@@ -32,12 +42,18 @@ def get_guid(address):
     return CurrentGUID
 
 def get_num_le(bytearr):
+    """
+    translate a set of bytes into a number in the little endian format
+    """
     num_le = 0
     for i in range(len(bytearr)):
         num_le += bytearr[i] * pow(256, i)
     return num_le
 
 def get_machine_type(header):
+    """
+    get the architecture of the investigated file
+    """
     PE_POINTER = header[PE_OFFSET]
     FH_POINTER = PE_POINTER + 4
     machine_type = header[FH_POINTER:FH_POINTER+2:]
@@ -49,8 +65,11 @@ def get_machine_type(header):
     return "unknown"
 
 def check_subsystem(header):
-        PE_POINTER = header[PE_OFFSET]
-        subsystem = header[PE_POINTER + 0x5c]
-        return (subsystem == IMAGE_SUBSYSTEM_EFI_APPLICATION or \
-                subsystem == IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER or \
-                subsystem == IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER)
+    """
+    get the subsystem of the investigated file
+    """
+    PE_POINTER = header[PE_OFFSET]
+    subsystem = header[PE_POINTER + 0x5c]
+    return (subsystem == IMAGE_SUBSYSTEM_EFI_APPLICATION or \
+            subsystem == IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER or \
+            subsystem == IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER)
