c0f4cc1c4be04d2e2c665e5d5f27990a6c1f9891
yeggor <vasilenko.yegor@gmail.com>
2021-04-13 02:18:51 +0300

    improved image extraction, added automatic search for Firmware Volume Header

--- tools/get_efi_images.py
+++ tools/get_efi_images.py
@@ -1,7 +1,7 @@
 ################################################################################
 # MIT License
 #
-# Copyright (c) 2018-2020 yeggor
+# Copyright (c) 2018-2021 yeggor
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,12 +22,12 @@
 # SOFTWARE.
 ################################################################################
 
+import glob
 import os
+import pathlib
+import re
 import shutil
-import sys
-from glob import glob
 
-import click
 import colorama
 import uefi_firmware
 
@@ -36,41 +36,8 @@ from .guid_db import UEFI_GUIDS
 DIR_NAME = 'all'
 PE_DIR = 'modules'
 
-
-def get_files(directory_name, pe_dir):
-    """get efi images from "all" directory"""
-    if not os.path.isdir(pe_dir):
-        os.mkdir(pe_dir)
-    bar_template = click.style('%(label)s  %(bar)s | %(info)s', fg='cyan')
-    files = os.listdir(directory_name)
-    label = 'Obtaining UEFI images'
-    with click.progressbar(files,
-                           length=len(files),
-                           bar_template=bar_template,
-                           label=label) as bar:
-        for obj in bar:
-            src = os.path.join(directory_name, obj)
-            template = os.path.join(directory_name, '*.ui')
-            if os.path.isfile(src):
-                if obj[-3:] == '.pe':
-                    if len(glob(template)) == 1:
-                        ui_path = glob(template)[0]
-                        with open(ui_path, 'rb') as ui:
-                            pe_name = ui.read().replace(b'\x00', b'')
-                            dst = os.path.join(pe_dir, pe_name.decode('utf-8'))
-                    else:
-                        # no UI section, try to get a friendly name from the GUID database
-                        pe_guid = directory_name.split(os.path.sep)[-2]
-                        pe_guid = pe_guid.replace("file-", "").upper()
-                        pe_name = UEFI_GUIDS.get(pe_guid)
-                        if not pe_name:
-                            # unknown GUID
-                            pe_name = pe_guid
-                        dst = os.path.join(pe_dir, pe_name)
-                    shutil.copy(src, dst)
-            if os.path.isdir(src):
-                get_files(src, pe_dir)
-    return True
+g_re_guid = re.compile(
+    r'file-[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}')
 
 
 class Dumper():
@@ -83,25 +50,73 @@ class Dumper():
         if not os.path.isdir(self.pe_dir):
             os.mkdir(self.pe_dir)
 
-    def dump_all(self):
+    @staticmethod
+    def _unsupported() -> bool:
+        print('[-] This type of binary is not supported')
+        return False
+
+    @staticmethod
+    def get_module_name(module_path: str) -> str:
+        module_name = str()
+        dir_name, _ = os.path.split(module_path)
+        template = os.path.join(dir_name, '*.ui')
+        if len(glob.glob(template)) == 1:
+            # try to get a friendly name from the *.ui file
+            ui_path = glob.glob(template)[0]
+            with open(ui_path, 'rb') as f:
+                module_name = f.read()
+            module_name = module_name.decode('utf-16le')
+            return module_name[:-1]
+        # no UI section, try to get a friendly name from the GUID database
+        file_guids = g_re_guid.findall(dir_name)
+        if not file_guids:
+            return str()
+        module_guid = file_guids[-1].replace('file-', '')
+        module_name = UEFI_GUIDS.get(module_guid.upper())
+        if not module_name:
+            module_name = module_guid
+        return module_name
+
+    @staticmethod
+    def search_pe(d: str) -> list:
+        return list(map(str, pathlib.Path(d).rglob('*.pe')))
+
+    @staticmethod
+    def search_te(d: str) -> list:
+        return list(map(str, pathlib.Path(d).rglob('*.te')))
+
+    def get_pe_files(self):
+        pe_files = self.search_pe(self.dir_name)
+        te_files = self.search_te(self.dir_name)
+        for module_path in (te_files + pe_files):
+            module_name = self.get_module_name(module_path)
+            if not module_name:
+                print(f'Current module: unknown')
+                continue
+            print(f'Current module: {module_name}')
+            dst = os.path.join(self.pe_dir, module_name)
+            shutil.copy(module_path, dst)
+
+    def dump_all(self) -> bool:
         if not os.path.isfile(self.fw_name):
             print(f'[-] Check {self.fw_name} file')
             return False
         with open(self.fw_name, 'rb') as fw:
             file_content = fw.read()
         parser = uefi_firmware.AutoParser(file_content)
-        if parser.type() == 'unknown':
-            print('[-] This type of binary is not supported')
-            return False
+        if parser.type() is 'unknown':
+            fvh_index = file_content.find(b'_FVH')
+            if fvh_index < 0:
+                return self._unsupported()
+            parser = uefi_firmware.AutoParser(file_content[fvh_index - 40:])
+            if parser.type() is 'unknown':
+                return self._unsupported()
         firmware = parser.parse()
         firmware.dump(self.dir_name)
         return True
 
-    def get_pe_files(self):
-        get_files(self.dir_name, self.pe_dir)
 
-
-def get_efi_images(fw_name):
+def get_efi_images(fw_name) -> bool:
     """get images from firmware"""
     colorama.init()  # for correct color display in uefi_firmware module
     dumper = Dumper(fw_name, DIR_NAME, PE_DIR)
